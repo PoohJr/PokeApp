1eeed00f7703fa8849f3cf0575dc7092b2a3292c
William Domenech <williamdomenech06@gmail.com>
2024-03-17 19:45:25 -0400

    got everything now style with tailwind

--- poke/src/App.jsx
+++ poke/src/App.jsx
@@ -1,7 +1,6 @@
 import Header from './pages/Header.jsx';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PokemonData from "./pages/PokemonData.jsx";
-import UserPoke from "./pages/UserPoke.jsx"; 
 // import NonLoad from './pages/NonLoad.jsx';
 
 
@@ -14,8 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Header />} /> 
         <Route path="/pokemonData" element={<PokemonData />} /> 
-        <Route path="/userPoke" element={<UserPoke />} /> 
-        
         {/* { <Route path="*" element={<NonLoad />} /> } */}
       </Routes>
     </BrowserRouter>
--- poke/src/pages/Header.jsx
+++ poke/src/pages/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
-import './Header.css';
 
 export function Header() {
     const [userInput, setUserInput] = useState("");
