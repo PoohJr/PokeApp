265d504c940b5834d5f280dc9b61cec307373d7f
William Domenech <williamdomenech06@gmail.com>
2024-03-18 16:32:59 -0400

    lil style

--- poke/src/pages/Header.jsx
+++ poke/src/pages/Header.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
+import {SearchIcon} from "@heroicons/vue/outline";
 
 export function Header() {
     const [userInput, setUserInput] = useState("");
@@ -28,26 +29,36 @@ export function Header() {
 
     return (
         <>
-            <div className="container">
-                <form>
-                    <div className="display">
-                        <h1 className="heading-text text-9xl ">PokeMon</h1>
+            <div className="flex w-100 h-screen ">
+                <form className=" flex flex-col w-full">
+                    <div className="text-center justify-center items-center h-3/4">
+                        <h1 className="text-9xl mt-28">PokeMon</h1>
+                        <SearchIcon className ="w-5 h-5"/>
                         <input
+                            className="mt-14 px-3 py-2 font-semibold placeholder-grey-500 text-black rounded-2xl border-none ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2"
                             id="in"
                             onChange={(e) => setUserInput(e.target.value)}
                             type="text"
                             placeholder="Choose Your Pokemon!"
+                            autoComplete="off"
+                            aria-label="Search Pokemon"
                         />
                         <button
-                            className="bg-blue-500"
+                            className=""
                             onClick={handleSubmit}
                         >
                             Search
                         </button>
                         <br />
-                        <button className="random">Randomize</button>
+                        <button 
+                        className="mt-16">
+                            Randomize
+                            </button>
                     </div>
                 </form>
+
+
+
             </div>
         </>
     );
