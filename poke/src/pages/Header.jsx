e5ea9b9e68f20c44b4ad50b1bf092f2bb91a6d0c
William Domenech <williamdomenech06@gmail.com>
2024-03-18 18:52:02 -0400

    lil update

--- poke/src/pages/Header.jsx
+++ poke/src/pages/Header.jsx
@@ -1,9 +1,9 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
-import {SearchIcon} from "@heroicons/vue/outline";
 
 export function Header() {
+    const [expoke, setexpokeData] = useState(null)
     const [userInput, setUserInput] = useState("");
     const [pokedata, setpokeData] = useState(null);
     const [newerror, setNewError] = useState(null);
@@ -26,16 +26,34 @@ export function Header() {
         }
         setUserInput("");
     };
+    useEffect(() =>{
+        async function example1() { 
+            try {
+                const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+                if (response.ok) {
+                    const data = await response.json();
+                    setexpokeData(data)
+                    console.log("Got Example 1:", data);
+                } else {
+                    console.error("Failed to fetch Example 1:", response.status);
+                }
+            } catch (error) {
+                console.error("Error fetching Example 1:", error);
+            }
+        }
+        example1()
+    },[])
+   
+   
 
     return (
         <>
-            <div className="flex w-100 h-screen ">
+            <div className="flex w-100 h-3/5 ">
                 <form className=" flex flex-col w-full">
                     <div className="text-center justify-center items-center h-3/4">
-                        <h1 className="text-9xl mt-28">PokeMon</h1>
-                        <SearchIcon className ="w-5 h-5"/>
+                        <h1 className="text-9xl mt-28 font-extrabold">PokeMon</h1>
                         <input
-                            className="mt-14 px-3 py-2 font-semibold placeholder-grey-500 text-black rounded-2xl border-none ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2"
+                            className="mt-16 mb-10 px-3 py-2 font-semibold placeholder-grey-500 text-black rounded-2xl border-none ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2"
                             id="in"
                             onChange={(e) => setUserInput(e.target.value)}
                             type="text"
@@ -44,22 +62,40 @@ export function Header() {
                             aria-label="Search Pokemon"
                         />
                         <button
-                            className=""
+                            className="hidden"
                             onClick={handleSubmit}
                         >
                             Search
                         </button>
                         <br />
                         <button 
-                        className="mt-16">
+                        className="mt-16 mb-20 bg-red-500 px-2 py-2 hover:bg-red-600 text-white rounded-full">
                             Randomize
                             </button>
                     </div>
                 </form>
-
-
-
             </div>
+            <div className="flex justify-center items-center">
+                <hr className="w-4/5 bg-grey-500 mt-6 "></hr>
+            </div>
+            <div className="flex justify-center">
+                <h1 className=" text-center text-3xl p-3 pt-5 font-semibold">PokeMon Examples</h1>
+            </div>
+
+           <div className="max-w-sm">
+                {expoke ? (
+                    <div className="bg-gray-300 h-fit ml-7 pl-5 rounded-md">
+                        <h2 className=" text-2xl"> id#: {expoke.id}</h2>
+                        <h2 className=" text-2xl">name: {expoke.name}</h2>
+                        <h2 className=" text-2xl">height: {expoke.height}</h2>
+                        <h2 className=" text-2xl">weight: {expoke.weight}</h2>
+                    </div>
+                    ) : (
+                        <p>Loading...</p>
+                    )}
+           </div>
+            
+
         </>
     );
 }
