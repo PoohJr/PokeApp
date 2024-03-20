4884982756ce6368f5bbe9431f8ff60d9c49cf1f
PoohJr <Williamdomenech06@gmail.com>
2024-03-20 13:57:58 -0400

    lil

---
+++ poke/src/Preloader.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import 'Preloader.css'
+function Preloader(){
+
+    <div className="bg-black ">
+        <div className="">
+            <img src="https://tenor.com/view/charmander-chases-tail-pokemon-nintendo-gif-16189806743405656732" alt="Charmander Being Cute" />
+        </div>
+    </div>
+}
+
+export default Preloader
