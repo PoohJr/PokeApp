60f4aebde80d925110f5a07f4b503a838dc2112c
PoohJr <Williamdomenech06@gmail.com>
2024-03-20 15:38:31 -0400

    before i leave work

--- poke/src/Preloader.jsx
+++ poke/src/Preloader.jsx
@@ -2,11 +2,11 @@ import React from "react"
 import 'Preloader.css'
 function Preloader(){
 
-    <div className="bg-black ">
-        <div className="">
-            <img src="https://tenor.com/view/charmander-chases-tail-pokemon-nintendo-gif-16189806743405656732" alt="Charmander Being Cute" />
-        </div>
-    </div>
+    <div className="bg-black w-full h-full">
+      <div className="w-35 h-35">
+          <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExajRva3Z2Znd6YjNieTZuYWdiNWJwbHRrenlneDIzZHc4Y2V4ZHdtcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/yhfTY8JL1wIAE/giphy.gif" alt="Charmander Being Cute" />
+      </div>
+  </div>
 }
 
 export default Preloader
