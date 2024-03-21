c76f7352b2180486d52d625d18be04aed6a54fd3
William Domenech <williamdomenech06@gmail.com>
2024-03-21 06:40:52 -0400

    change
    big changes gonna chnage it up a bit

--- poke/src/Example.jsx
+++ poke/src/Example.jsx
@@ -1,98 +1,17 @@
-import React, { useState, useEffect, createElement } from "react";
+import React, { useState, useEffect, } from "react";
 
 
 function Example(){
     const [pokeEva, setpokeEva] = useState(null)
     const [expoke, setexpokeData] = useState(null)
-    const Playaudio = () => { 
-        if(expoke){
-        new Audio(`https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/1.ogg`).play()
-        }
-    }
-
     function firstLetter(string){
         return string.charAt(0).toUpperCase() + string.slice(1); 
     }
 
-    function TypeColor(typename){
-        switch(typename){
-            case "grass":
-                return "text-[#7AC74C]"
-            case "poison":
-                return "text-[#A33EA1]"
-            case "fire":
-                return "text-[#EE8130]"
-            case "normal":
-                return "text-[#A8A77A]"
-            case "water":
-                return "text-[#6390F0]"
-            case "electric":
-                return "text-[#F7D02C]"
-            case "ice":
-                 return "text-[#96D9D6]"
-            case "fighting":
-                 return "text-[#C22E28]"
-            case "ground":
-                 return "text-[#E2BF65]"
-            case "flying":
-                return "text-[#A98FF3]"
-            case "psychic":
-                return "text-[#F95587]"
-            case "bug":
-                return "text-[#A6B91A]"
-            case "rock":
-                return "text-[#B6A136]"
-            case "ghost":
-                return "text-[#735797]"
-            case "dragon":
-                return "text-[#6F35FC]"
-            case "dark":
-                return "text-[#705746]"
-            case "steel":
-                return "text-[#B7B7CE]"
-            case "fairy":
-                return "text-[#D685AD]"
-        }
-    }
-    function weightConver(){
 
-    }
 
-    function heightConver(){
-
-    }
     
-    function Progressbar(item, base ){
-        
-        switch (item){
-            case "hp":
-            return (
-                <>
-        <div className="flex justify-between mb-1">
-            <span className={`text-base font-medium text-red-700`}>{item}:</span>
-            <span className="text-sm font-medium text-red-700">{base}%</span>
-        </div>   
-                </>
-            );
-            case "defense":
-                return (
-                    <>
-            <div className="flex justify-between mb-1">
-                <span className={`text-base font-medium text-blue-700`}>{item}:</span>
-                <span className="text-sm font-medium text-blue-700">{base}%</span>
-            </div>   
-                    </>
-                );
-            default:
-                return null;
-        }
-        // <div className="flex justify-between mb-1">
-        //     <span className={`text-base font-medium text-black-700 dark:text-white`}>{item}</span>
-        //     <span className="text-sm font-medium text-{}-700 dark:text-white">{item}</span>
-        // </div>        
 
-       
-    }
 
 
     useEffect(() =>{
@@ -118,56 +37,29 @@ function Example(){
         Sample()
     },[])
 
-    return(
+    return (
         <>
             <div className="flex justify-center items-center">
                 <hr className="w-4/5 bg-grey-500 mt-6 "></hr>
             </div>
             <div className="flex justify-center">
-                <h1 className=" text-center text-3xl p-3 pt-5 font-semibold">PokeMon Examples</h1>
+                <h1 className=" text-center text-3xl p-3 pt-5 font-semibold">PokeMon Dex</h1>
             </div>
 
-           <div className="w-96 bg-gray-300 h-35 m-5 pl-5 rounded-md p-4 mb-7">
+           <div className="max-w-64 bg-gray-300 max-h-fit m-10 rounded-md p-4">
                 {expoke ? (
-                    <div className="flex flex-col">
-                        <div className="flex justify-between">
-                            <p className="  "> id#: {expoke.id}</p>
-                            <div className="inline-flex">
-                                <p className="px-2"></p>
-                                <ul className="">
-                                    {expoke.types.map((type, index) => (
-                                        <li className={`"inline p-1.5 text-xl" ${TypeColor(type.type.name)}`}key={index}>{type.type.name}</li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-                        
-                            <div className="flex justify-center ">
-                                    <p className="text-4xl">{firstLetter(expoke.name)}</p>
-                            </div>
-                            <div className="flex justify-center ">
-                                 <img className="h-64 w-64" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png`} alt="Pokemon Image"/>
-                            </div>
-                        <p className="text-bold">Height: {expoke.height}</p>
-                        <p className="">Weight: {expoke.weight}</p>
+                    <div className="flex flex-col h-55">
 
-                        <p>Stats </p>
-                    <ul>
-                        {expoke.stats.map((stat, index) => (
-                            <li key={index}> 
-                            {Progressbar(stat.stat.name, stat.base_stat)}
-                                     </li>
+                        <div className="flex flex-col justify-center">
                             
-                            ))}
-                    </ul>   
-                    <button onClick={Playaudio}>PokeMon Cry: </button> 
-                        <div className="inline-flex">
-                            <p>Abilitlies:</p>
-                            <ul className="">
-                                {expoke.abilities.map((ability, index) => (
-                                    <li className="inline px-3" key={index}>{ability.ability.name}</li>
-                                ))}
-                            </ul>
+                                <div className="flex justify-center ">
+                                        <p className="text-2xl">{firstLetter(expoke.name)}</p>
+                                </div>
+                                <div className=" flex justify-center rounded-full h-12 w-12 bg-black items-center text-center"></div>
+                                <div className=" h-14 w-40">
+                                    <img className="p-4" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png`} alt="Pokemon Image"/>
+                                </div>
+                            
                         </div>
 
                     </div>
@@ -178,5 +70,8 @@ function Example(){
         </>
     )
 }
+// idea put pokemon favicon in the corner of every poke card
+
+
 
 export default Example
--- poke/src/pages/PokemonData.jsx
+++ poke/src/pages/PokemonData.jsx
@@ -8,25 +8,107 @@ function PokemonData() {
     const location = useLocation();
     const pokedata = location.state ? location.state.pokedata : null;
     console.log(location.state.pokedata)
+    
     const Playaudio = () => { 
         if(pokedata){
         new Audio(`https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${pokedata.id}.ogg`).play()
         }
     }
+    function weightConver(){
 
+    }
+
+    function heightConver(){
+    }
+        function TypeColor(typename){
+        switch(typename){
+            case "grass":
+                return "text-[#7AC74C]"
+            case "poison":
+                return "text-[#A33EA1]"
+            case "fire":
+                return "text-[#EE8130]"
+            case "normal":
+                return "text-[#A8A77A]"
+            case "water":
+                return "text-[#6390F0]"
+            case "electric":
+                return "text-[#F7D02C]"
+            case "ice":
+                 return "text-[#96D9D6]"
+            case "fighting":
+                 return "text-[#C22E28]"
+            case "ground":
+                 return "text-[#E2BF65]"
+            case "flying":
+                return "text-[#A98FF3]"
+            case "psychic":
+                return "text-[#F95587]"
+            case "bug":
+                return "text-[#A6B91A]"
+            case "rock":
+                return "text-[#B6A136]"
+            case "ghost":
+                return "text-[#735797]"
+            case "dragon":
+                return "text-[#6F35FC]"
+            case "dark":
+                return "text-[#705746]"
+            case "steel":
+                return "text-[#B7B7CE]"
+            case "fairy":
+                return "text-[#D685AD]"
+        }
+    }
+        function Progressbar(item, base ){
+        
+        switch (item){
+            case "hp":
+            return (
+                <>
+        <div className="flex justify-between mb-1">
+            <span className={`text-base font-medium text-red-700`}>{item}:</span>
+            <span className="text-sm font-medium text-red-700">{base}%</span>
+        </div>   
+                </>
+            );
+            case "defense":
+                return (
+                    <>
+            <div className="flex justify-between mb-1">
+                <span className={`text-base font-medium text-blue-700`}>{item}:</span>
+                <span className="text-sm font-medium text-blue-700">{base}%</span>
+            </div>   
+                    </>
+                );
+            default:
+                return null;
+        }
+        // <div className="flex justify-between mb-1">
+        //     <span className={`text-base font-medium text-black-700 dark:text-white`}>{item}</span>
+        //     <span className="text-sm font-medium text-{}-700 dark:text-white">{item}</span>
+        // </div>        
+
+       
+        }
+    
     return (
         <div>
             {pokedata && (
                 <div>
-                    <h2 className="text-red-400">Name: {pokedata.name}</h2>
-                    <p>Poke#: {pokedata.id}</p>
                     <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokedata.id}.png`} alt="Pokemon Image"/>
-                    <p>Type:</p>
-                        <ul>
-                             {pokedata.types.map((type, index) => (
-                                <li key={index}>{type.type.name}</li>
-                             ))}
-                        </ul>
+                    <h2 className="text-red-400">Name: {pokedata.name}</h2>
+                    <div className="flex justify-between">
+                            <p className="  "> id#: {pokedata.id}</p>
+                            <div className="inline-flex">
+                                <p className="px-2"></p>
+                                <ul className="">
+                                    {pokedata.types.map((type, index) => (
+                                        <li className={`"inline p-1.5 text-xl" ${TypeColor(type.type.name)}`}key={index}>{type.type.name}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
                     <p>Height: {pokedata.height}</p>
                     <p>Weight: {pokedata.weight}</p>
                     <button onClick={Playaudio}>PokeMon Cry: </button> 
@@ -40,19 +122,23 @@ function PokemonData() {
                         ))}
                     </ul> */}
 
-                    <p>Stats: </p>
-                    <ul>
-                        {pokedata.stats.map((stat, index) => (
-                            <li key={index}>{stat.stat.name} {stat.base_stat}</li>
-                            ))}
-                    </ul>   
-
-                    <p>Abilitlies:</p>
-                    <ul>
-                        {pokedata.abilities.map((ability, index) => (
-                            <li key={index}>{ability.ability.name}</li>
-                        ))}
-                    </ul>
+                    <p>Stats </p>
+                        <ul>
+                            {pokedata.stats.map((stat, index) => (
+                                <li key={index}> 
+                                {Progressbar(stat.stat.name, stat.base_stat)}
+                                        </li>
+                                
+                                ))}
+                        </ul> 
+                        <div className="inline-flex">
+                            <p>Abilitlies:</p>
+                            <ul className="">
+                                {pokedata.abilities.map((ability, index) => (
+                                    <li className="inline px-3" key={index}>{ability.ability.name}</li>
+                                ))}
+                            </ul>
+                        </div>
 
                     
                 </div>
