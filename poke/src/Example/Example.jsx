4802c98f9059b62d857b6a36b929619b993046c7
William Domenech <williamdomenech06@gmail.com>
2024-03-23 20:05:52 -0400

    lil

--- poke/src/Example/Example.jsx
+++ poke/src/Example/Example.jsx
@@ -47,9 +47,16 @@ function Example() {
     
 
 
-    const ClickPoke =  async() => {
+    const handleClickPokemon =  async() => {
             try{
-                
+                const gettinPoke = await Promise.all(
+                    kantoPokemon.map(async(pokemon) => {
+                        const res = await pokemon.url;
+                        const data = res.json()
+                        setclickpokedata(data)
+                        
+                    })
+                )
             } catch{
                 console.error(error + "That Was the error")
             }
@@ -60,14 +67,14 @@ function Example() {
     return (
         <>
             {
-                <div className=" flex flex-wrap bg-gray-400 w-full ">
+                <div className=" flex flex-wrap bg-gray-400 w-full mt-40">
                     {kantoPokemon.map((pokemon, index) => (
                     <div className="flex flex-col w-2/12 h-40 bg-red-500 rounded-full m-5 " key={index}>
                             <span className="text-md text-center mt-2 text-white">{capitalizeFirstLetter(pokemon.name)}</span>
                             <div className="flex justify-center">
                                 <div className="bg-white h-28 w-18 rounded-full border-8 border-red-600 ">
                                     <div className="h-full flex items-center justify-center">
-                                        <img onClick={ClickPoke} className="max-h-24 hover:-translate-y-3 ease-in-out duration-200 cursor-pointer"
+                                        <img onClick={handleClickPokemon} className="max-h-24 hover:-translate-y-3 ease-in-out duration-200 cursor-pointer"
                                             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
                                             alt={pokemon.name} />
                                     </div>
@@ -81,3 +88,7 @@ function Example() {
 }
 
 export default Example;
+        // BackGround images
+        // https://assets.pokemon.com//assets/cms2/img/misc/virtual-backgrounds/sword-shield/dynamax-battle.png
+        //https://assets.pokemon.com//assets/cms2/img/misc/virtual-backgrounds/sword-shield/pokemon-in-the-wild.png
+       //https://assets.pokemon.com//assets/cms2/img/misc/virtual-backgrounds/tcg/sun-moon-cosmic-eclipse-01.jpg
--- poke/src/pages/Header.jsx
+++ poke/src/pages/Header.jsx
@@ -45,33 +45,34 @@ export function Header() {
                 <Preloader/>
             ) : (
                 <>
-                    <div className="flex w-full h-3/5 ">
-                        <form className="flex flex-col w-full">
-                            <div className="text-center justify-center items-center h-3/4">
-                                <h1 className="text-9xl mt-28 font-extrabold">PokeMon</h1>
-                                <input
-                                    className="mt-16 mb-10 px-3 py-2 font-semibold placeholder-grey-500 text-black rounded-2xl border-none ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2"
-                                    id="in"
-                                    onChange={(e) => setUserInput(e.target.value)}
-                                    type="text"
-                                    placeholder="Choose Your Pokemon!"
-                                    autoComplete="off"
-                                    aria-label="Search Pokemon"
-                                />
-                                <button
-                                    className="hidden"
-                                    onClick={handleSubmit}
-                                >
-                                    Search
-                                </button>
-                                <br />
-                                <button 
-                                    className="text-xl mt-16 mb-20 bg-red-500 px-2 py-2 hover:bg-red-600 text-white rounded-full">
-                                    Randomize
-                                </button>
-                            </div>
-                        </form>
+                    <div className=" bg-cover bg-center h-96 z-0" style={{backgroundImage: 
+                        "url('https://assets.pokemon.com//assets/cms2/img/misc/virtual-backgrounds/sword-shield/pokemon-in-the-wild.png')"}}>
+                        <div className="flex w-full h-3/5 bg-[url('/bg-img/pokemon-in-the-wild.png)]">
+                            <form className="flex flex-col w-full">
+                                <div className="text-center justify-center items-center h-3/4">
+                                    <h1 className="text-9xl mt-28 font-extrabold">PokeMon</h1>
+                                    <input
+                                        className="mt-16 mb-10 px-3 py-2 font-semibold placeholder-grey-500 text-black rounded-2xl border-none ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2"
+                                        id="in"
+                                        onChange={(e) => setUserInput(e.target.value)}
+                                        type="text"
+                                        placeholder="Choose Your Pokemon!"
+                                        autoComplete="off"
+                                        aria-label="Search Pokemon"
+                                    />
+                                    <button
+                                        className="hidden"
+                                        onClick={handleSubmit}
+                                    >
+                                        Search
+                                    </button>
+                                    <br />
+
+                                </div>
+                            </form>
+                        </div>
                     </div>
+
                     <Example/>
                 </>
             )}
