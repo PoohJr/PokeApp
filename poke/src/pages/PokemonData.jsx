887fec32a7c039c047f8a4b6c19881887db3b94a
William Domenech <williamdomenech06@gmail.com>
2024-03-25 19:52:39 -0400

    gotta work around since function not workin

--- poke/src/pages/PokemonData.jsx
+++ poke/src/pages/PokemonData.jsx
@@ -18,46 +18,50 @@ function PokemonData() {
 
     }
 
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
     function heightConver(){
     }
         function TypeColor(typename){
         switch(typename){
             case "grass":
-                return "text-[#7AC74C]"
+                return "bg-[#7AC74C] "
             case "poison":
-                return "text-[#A33EA1]"
+                return "bg-[#A33EA1]"
             case "fire":
-                return "text-[#EE8130]"
+                return "bg-[#EE8130]"
             case "normal":
-                return "text-[#A8A77A]"
+                return "bg-[#A8A77A]"
             case "water":
-                return "text-[#6390F0]"
+                return "bg-[#6390F0]"
             case "electric":
-                return "text-[#F7D02C]"
+                return "bg-[#F7D02C]"
             case "ice":
-                 return "text-[#96D9D6]"
+                 return "bg-[#96D9D6]"
             case "fighting":
-                 return "text-[#C22E28]"
+                 return "bg-[#C22E28]"
             case "ground":
-                 return "text-[#E2BF65]"
+                 return "bg-[#E2BF65]"
             case "flying":
-                return "text-[#A98FF3]"
+                return "bg-[#A98FF3]"
             case "psychic":
-                return "text-[#F95587]"
+                return "bg-[#F95587]"
             case "bug":
-                return "text-[#A6B91A]"
+                return "bg-[#A6B91A]"
             case "rock":
-                return "text-[#B6A136]"
+                return "bg-[#B6A136]"
             case "ghost":
-                return "text-[#735797]"
+                return "bg-[#735797]"
             case "dragon":
-                return "text-[#6F35FC]"
+                return "bg-[#6F35FC]"
             case "dark":
-                return "text-[#705746]"
+                return "bg-[#705746]"
             case "steel":
-                return "text-[#B7B7CE]"
+                return "bg-[#B7B7CE]"
             case "fairy":
-                return "text-[#D685AD]"
+                return "bg-[#D685AD]"
         }
     }
         function Progressbar(item, base ){
@@ -67,8 +71,8 @@ function PokemonData() {
             return (
                 <>
         <div className="flex justify-between mb-1">
-            <span className={`text-base font-medium text-red-700`}>{item}:</span>
-            <span className="text-sm font-medium text-red-700">{base}%</span>
+            <span className={`bg-base font-medium bg-red-700`}>{item}:</span>
+            <span className="bg-sm font-medium bg-red-700">{base}%</span>
         </div>   
                 </>
             );
@@ -76,8 +80,8 @@ function PokemonData() {
                 return (
                     <>
             <div className="flex justify-between mb-1">
-                <span className={`text-base font-medium text-blue-700`}>{item}:</span>
-                <span className="text-sm font-medium text-blue-700">{base}%</span>
+                <span className={`bg-base font-medium bg-blue-700`}>{item}:</span>
+                <span className="bg-sm font-medium bg-blue-700">{base}%</span>
             </div>   
                     </>
                 );
@@ -85,33 +89,43 @@ function PokemonData() {
                 return null;
         }
         // <div className="flex justify-between mb-1">
-        //     <span className={`text-base font-medium text-black-700 dark:text-white`}>{item}</span>
-        //     <span className="text-sm font-medium text-{}-700 dark:text-white">{item}</span>
+        //     <span className={`bg-base font-medium bg-black-700 dark:bg-white`}>{item}</span>
+        //     <span className="bg-sm font-medium bg-{}-700 dark:bg-white">{item}</span>
         // </div>        
 
        
         }
     
     return (
-        <div>
+        <div className="p-10 h-full">
             {pokedata && (
-                <div>
-                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokedata.id}.png`} alt="Pokemon Image"/>
-                    <h2 className="text-red-400">Name: {pokedata.name}</h2>
+                <div className="">
+                    <div className="flex  mt-16">
+                        <h2 className="text-5xl ">{capitalizeFirstLetter(pokedata.name)}</h2>
+                        <button className=" ml-3" onClick={Playaudio}>Icon</button> 
+                        <ul className="flex">
+                                        {pokedata.types.map((type, index) => (
+                                            <li className={`" mx-3 p-4 bg-lg  text-white rounded-sm" ${TypeColor(type.type.name)}`}key={index}>{type.type.name}</li>
+                                        ))}
+                                    </ul>
+                    </div>
+
+                        <div className=" flex  rounded w-full mt-8">
+                            <p className="  "> PokeMon #: {pokedata.id}</p>
+                            <p className="">{pokedata.location_area_encounters}</p>
+                            <img  className ="ml-auto mr-20 motion-safe:animate-bounce justify-end h-96 "src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokedata.id}.png`} alt="Pokemon Image"/>
+                        </div>
+                    
+                <table>
                     <div className="flex justify-between">
-                            <p className="  "> id#: {pokedata.id}</p>
+                            
                             <div className="inline-flex">
-                                <p className="px-2"></p>
-                                <ul className="">
-                                    {pokedata.types.map((type, index) => (
-                                        <li className={`"inline p-1.5 text-xl" ${TypeColor(type.type.name)}`}key={index}>{type.type.name}</li>
-                                    ))}
-                                </ul>
+
                             </div>
                         </div>
                     <p>Height: {pokedata.height}</p>
                     <p>Weight: {pokedata.weight}</p>
-                    <button onClick={Playaudio}>PokeMon Cry: </button> 
+                    
                     <p>Where ya can find em : {pokedata.location_area_encounters}</p>
 
                     <p>Moves:</p>
@@ -135,10 +149,13 @@ function PokemonData() {
                             <p>Abilitlies:</p>
                             <ul className="">
                                 {pokedata.abilities.map((ability, index) => (
-                                    <li className="inline px-3" key={index}>{ability.ability.name}</li>
+                                    <tr key={index} >
+                                    <li className="inline px-3" >{ability.ability.name}</li>
+                                    </tr>
                                 ))}
                             </ul>
                         </div>
+                    </table>
 
                     
                 </div>
