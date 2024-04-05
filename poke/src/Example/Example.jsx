2c2cd5d02f70aa4896a0e4a82a3c7841a8780343
William Domenech <williamdomenech06@gmail.com>
2024-04-04 20:39:37 -0400

    Update Example.jsx

--- poke/src/Example/Example.jsx
+++ poke/src/Example/Example.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Example() {
     const [kantoPokemon, setKantoPokemon] = useState([]);
     const [clickpokedata, setclickpokedata] = useState()
 
+    const navigate = useNavigate();
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
@@ -104,7 +106,7 @@ function Example() {
                     navigate("./Clicked", {state: {clickpokedata: res.data} });
                     console.log(gettinPoke)
                 }) 
-            } catch{
+            } catch(error){
                 console.error(error + "That Was the error")
             }
               
@@ -117,11 +119,11 @@ function Example() {
                 <div className=" flex flex-wrap  justify-evenly w-full ">
                     {kantoPokemon.map((pokemon, index) => (
                     <div className="flex flex-col  w-96 h-52 bg-red-500 rounded mt-8 m-3 " key={index}>
-                            <div className="text-md text-center bg-black  mx-20 mt-3 mb-3">
+                            <div className="text-md text-center bg-black rounded-lg mx-36 mt-3 mb-3">
                                 <span className=" mt-2 text-white before:content-[]">{capitalizeFirstLetter(pokemon.name)}</span>
                             </div>
                             <div className="flex justify-center">
-                                <div className="bg-white h-28 w-40 rounded-full border-8 border-red-600 ">
+                                <div className="bg-white h-36 w-40 rounded-full border-8 border-red-600 ">
                                     <div className="h-full flex items-center justify-center">
                                         <img onClick={ClickPoke} className="max-h-24 hover:-translate-y-3 ease-in-out duration-200 cursor-pointer"
                                             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
