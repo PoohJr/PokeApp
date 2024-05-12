0ec5b941f3cc96f29459b3f8a82c893f06c38b00
PoohJr <Williamdomenech06@gmail.com>
2024-05-12 15:25:21 -0400

    Update Bug.jsx

--- poke/src/TypePage/Bug.jsx
+++ poke/src/TypePage/Bug.jsx
@@ -39,26 +39,30 @@ function Bug (){
 {typeinfo &&(
     
     <div className="px-8">
-        <div className="flex pt-8">
+        <div className="flex pt-8 justify-between">
             <div className="bg-white h-52 border-black border-2 rounded-2xl">
                 <img className={`content-${typeinfo.name} h-40`}  alt="Pokemon Img" />
                 <p className="text-center font-bold">{capitalizeFirstLetter(typeinfo.name)}</p>
             </div>
-            <div className="flex border-2 border-black h-96 bg-white ">
-                <p className="text-end h-10">Weak To:</p>
-                {typeinfo.damage_relations.double_damage_from.map((e ,index ) => (
-                    <div key={index} className="">
-                    <img className={`px-2 h-20 content-${e.name}`} src="" alt="" />
-                        <p className="text-center font-semibold">{capitalizeFirstLetter(e.name)}</p>
-                        </div>
-                ))}
-                <p>Strong Aginainst:</p>
-                {typeinfo.damage_relations.double_damage_to.map((e ,index ) => (
-                    <div key={index} className="">
-                    <img className={`px-2 h-20 content-${e.name}`} src="" alt="" />
-                        <p className="text-center font-semibold">{capitalizeFirstLetter(e.name)}</p>
-                        </div>
-                ))}
+            <div className="flex flex-wrap border-2 border-black h-50 bg-white ">
+                <div className="flex flex-row h-20">
+                    <p className="text-end h-10">Weak To:</p>
+                    {typeinfo.damage_relations.double_damage_from.map((e ,index ) => (
+                        <div key={index} className="">
+                        <img className={`px-2 h-20 content-${e.name}`} src="" alt="" />
+                            <p className="text-center font-semibold">{capitalizeFirstLetter(e.name)}</p>
+                            </div>
+                    ))}
+                </div>
+                <div className="flex flex-row ">
+                    <p className="text-center">Strong Against:</p>
+                    {typeinfo.damage_relations.double_damage_to.map((e ,index ) => (
+                        <div key={index} className="h-20">
+                        <img className={`px-2 h-20 content-${e.name}`} src="" alt="" />
+                            <p className="text-center font-semibold">{capitalizeFirstLetter(e.name)}</p>
+                            </div>
+                    ))}
+                </div>
             </div>
         </div>
 
