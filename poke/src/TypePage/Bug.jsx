b37b77a3b67ad2224d335b987df7d544b0d1e7f9
PoohJr <Williamdomenech06@gmail.com>
2024-05-12 12:55:42 -0400

    Update Bug.jsx

--- poke/src/TypePage/Bug.jsx
+++ poke/src/TypePage/Bug.jsx
@@ -4,23 +4,25 @@ import { useEffect, useState } from "react";
 
 function Bug (){
     const [typeinfo, settypeinfo] = useState([])
-
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
 
     useEffect(() =>  {
-        async() =>{
+        const FetchType =  async() =>{
         try{
             // C Type in axios
-            const get = axios.get("https://pokeapi.co/api/v2/type/bug/") 
-            console.log(get)
+            const get = await axios.get("https://pokeapi.co/api/v2/type/bug/") 
             const res = get.data
-            console.log(res)
+
             settypeinfo(res)
-            console.log(res)
+   
 
         } catch(error){
             
             }
         }
+        FetchType()
 
     },[])
 
@@ -28,8 +30,23 @@ function Bug (){
 
 
     return(<>
-        <h1> Welcome the Bug Type page </h1>
-        <h1>{typeinfo}</h1>
+        {console.log(typeinfo)}
+    <div className="h-full">
+    <h1 className="text-center"> Welcome the {capitalizeFirstLetter(typeinfo.name)} Type page </h1>
+        <div className="flex ml-8">
+            <img className={`content-${typeinfo.name} h-40`}  alt="Pokemon Img" />
+            <div className="justify-end">
+            <p>Weak To:</p>
+               {typeinfo.damage_relations.double_damage_from.map((e ,index ) => (
+                <div key={index} className="">
+                <img className={`h-20 content-${e.name}`} src="" alt="" />
+                    <p className="text-center">{capitalizeFirstLetter(e.name)}</p>
+                    </div>
+               ))}
+            </div>
+        </div>
+        <p>{capitalizeFirstLetter(typeinfo.name)}</p>
+    </div>
         </>)
 }
 
